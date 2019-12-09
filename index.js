38eb847b731456fc34df7adb35065265de2460be
hdp2112 <hdp2112@columbia.edu>
2019-12-09 17:48:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const logDriverNames = function (drivers) {
+    return drivers.filter(function (driver) {
+        return console.log(`${driver.name}`);
+    });
+}
+
+const logDriversByHometown = function (drivers, hometown) {
+    return drivers.filter(function (driver) {
+        if ( driver.hometown === hometown ) {
+            return console.log(`${driver.name}`);
+        };
+    });
+}
+
+let driversByRevenue = function (drivers) {
+    return drivers.slice().sort(function (drivera, driverb) {
+        return drivera.revenue - driverb.revenue;
+    });
+}
+
+let driversByName = function (drivers) {
+    return drivers.slice().sort(function (drivera, driverb) {
+        return drivera.name.localeCompare(driverb.name);
+    });
+}
+
+let totalRevenue = function (drivers) {
+    return drivers.reduce(function (total, driver) {
+        return driver.revenue + total;
+    }, 0);
+}
+
+let averageRevenue = function (drivers) {
+    return totalRevenue(drivers) / drivers.length;
+}
